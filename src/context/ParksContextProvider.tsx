e7b1834e458e2bbd105b32613f825e6abc07214e
esterpratt <esterpratt@gmail.com>
2024-04-16 21:45:10 +0300

    use mock data for parks

--- src/context/ParksContextProvider.tsx
+++ src/context/ParksContextProvider.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, createContext, useEffect, useState } from 'react';
-import { db } from '../firebase-config';
-import { getDocs, collection } from 'firebase/firestore';
+// import { db } from '../firebase-config';
+// import { getDocs, collection } from 'firebase/firestore';
+import { mockParks } from '../mockParks';
 import { Park } from '../types/park';
 
 interface ParksContextObj {
@@ -15,23 +16,28 @@ const ParksContext = createContext<ParksContextObj>(initialData);
 
 const ParksContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [parks, setParks] = useState<Park[]>([]);
-  const parksCollection = collection(db, 'parks');
+  // const parksCollection = collection(db, 'parks');
 
-  useEffect(() => {
-    const getParks = async () => {
-      const data = await getDocs(parksCollection);
-      const parks = data.docs.map((doc) => {
-        return {
-          ...doc.data(),
-          id: doc.id,
-          location: doc.data().location.toJSON(),
-        };
-      }) as Park[];
-      setParks(parks);
-    };
+  useEffect(
+    () => {
+      const getParks = async () => {
+        // const data = await getDocs(parksCollection);
+        // const parks = data.docs.map((doc) => {
+        //   return {
+        //     ...doc.data(),
+        //     id: doc.id,
+        //     location: doc.data().location.toJSON(),
+        //   };
+        // }) as Park[];
+        setParks(mockParks);
+      };
 
-    getParks();
-  }, [parksCollection]);
+      getParks();
+    },
+    [
+      /*parksCollection*/
+    ]
+  );
 
   const value: ParksContextObj = {
     parks,
