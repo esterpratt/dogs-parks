60ef24a9af9d87f015300a6fa0d5d40b36a4dc2c
esterpratt <esterpratt@gmail.com>
2024-05-11 23:27:24 +0300

    settings page

--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,9 +1,14 @@
-import { useContext } from 'react';
-import { UserContext } from '../context/UserContext';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router';
+import { IconContext } from 'react-icons';
+import { MdLogout, MdOutlineModeEditOutline } from 'react-icons/md';
+import { UserContext } from '../context/UserContext';
+import { EditUserModal } from '../components/profile/EditUserModal';
+import styles from './UserInfo.module.scss';
 
 const UserInfo = () => {
   const { userLogout } = useContext(UserContext);
+  const [isEditUserModalOpen, setIsEditDogsModalOpen] = useState(false);
   const navigate = useNavigate();
 
   const logout = () => {
@@ -11,7 +16,33 @@ const UserInfo = () => {
     navigate('/');
   };
 
-  return <div onClick={logout}>Logout</div>;
+  const onEditUser = () => {
+    setIsEditDogsModalOpen(true);
+  };
+
+  const onCloseUserModal = () => {
+    setIsEditDogsModalOpen(false);
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div onClick={onEditUser} className={styles.row}>
+          <IconContext.Provider value={{ className: styles.icon }}>
+            <MdOutlineModeEditOutline />
+          </IconContext.Provider>
+          <span>Edit Details</span>
+        </div>
+        <div onClick={logout} className={styles.row}>
+          <IconContext.Provider value={{ className: styles.icon }}>
+            <MdLogout />
+          </IconContext.Provider>
+          <span>Logout</span>
+        </div>
+      </div>
+      <EditUserModal isOpen={isEditUserModalOpen} onClose={onCloseUserModal} />
+    </>
+  );
 };
 
 export { UserInfo };
