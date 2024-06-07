1fa3b70cf55ff5a49b70663025fcb9085c653599
esterpratt <esterpratt@gmail.com>
2024-06-07 15:49:03 +0300

    prevent scroll when input focused to deal with safari viewport issue

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -2,8 +2,11 @@ import { Outlet } from 'react-router';
 import { NavbarTop } from './components/NavbarTop';
 import { NavbarBottom } from './components/NavbarBottom';
 import styles from './RootLayout.module.scss';
+import { usePreventScrollOnFocus } from './hooks/usePreventScrollOnFocus';
 
 const RootLayout = () => {
+  usePreventScrollOnFocus();
+
   return (
     <>
       <NavbarTop />
