b7fb3b24ed3db78e7456537b24d532fcdec790dc
esterpratt <esterpratt@gmail.com>
2025-09-03 20:46:34 +0300

    exclude e2e tests from npm test

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    exclude: [
+      'tests/e2e/**/*',
+      'node_modules',
+      'dist'
+    ],
   },
 });
