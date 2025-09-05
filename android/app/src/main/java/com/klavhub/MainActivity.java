c985c151cc8eb4dfd972b7b1aa30ab2e8c17e136
esterpratt <esterpratt@gmail.com>
2025-09-05 20:36:37 +0300

    update android

--- android/app/src/main/java/com/klavhub/MainActivity.java
+++ android/app/src/main/java/com/klavhub/MainActivity.java
@@ -4,9 +4,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.graphics.Color;
+
+import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -14,21 +13,8 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Window window = getWindow();
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      window.setDecorFitsSystemWindows(false);
-    }
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      window.setNavigationBarColor(Color.TRANSPARENT);
-    }
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      window.getDecorView().setSystemUiVisibility(
-        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-      );
-    }
+    EdgeToEdge.enable(this);
 
     createDefaultNotificationChannel();
   }
