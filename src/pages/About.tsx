7bd28ab3a40c1e3c215e4f7af992325c9bdff17f
esterpratt <esterpratt@gmail.com>
2025-09-22 12:24:25 +0300

    implement translations for about, privacy-policy, delete-account pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,24 +1,22 @@
 import { Link } from 'react-router-dom';
 import { Button } from '../components/Button';
+import { useTranslation } from 'react-i18next';
 import styles from './About.module.scss';
 
 const MAIL = 'esterpratt@gmail.com';
 
 const About = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.container}>
       <div>
-        <h3>Welcome to KlavHub!</h3>
-        <span>
-          KlavHub is built for dog lovers looking to find the best nearby dog
-          parks and connect with friends.
-        </span>
+        <h3>{t('about.welcomeTitle')}</h3>
+        <span>{t('about.welcomeBody')}</span>
       </div>
       <div>
-        <h2>Need help or have feedback?</h2>
+        <h2>{t('about.helpTitle')}</h2>
         <span>
-          If you're experiencing issues with the app or have any suggestions,
-          feel free to reach out at:{' '}
+          {t('about.helpBody')}{' '}
           <Link to={`mailto:${MAIL}`}>
             <Button className={styles.button} variant="simple">
               {MAIL}.
@@ -29,36 +27,31 @@ const About = () => {
       <div>
         <Link to="/privacy-policy">
           <Button variant="simple" className={styles.button}>
-            View our privacy policy here.
+            {t('about.privacyCta')}
           </Button>
         </Link>
       </div>
       <div>
-        <span>
-          This app is just getting started, and your help growing the community
-          is deeply appreciated. You can support Klavhub by joining the
-          community, sharing with friends, adding details about your local dog
-          park, and contributing missing parks.
-        </span>
+        <span>{t('about.earlyBody')}</span>
       </div>
       <div>
-        <span>Thank you!</span>
+        <span>{t('about.thankYou')}</span>
       </div>
       <div>
-        <span>Created by: </span>
+        <span>{t('about.createdBy')} </span>
         <Link to="https://github.com/esterpratt">
           <Button className={styles.button} variant="simple">
-            Ester Pratt.
+            {t('about.createdByName')}
           </Button>
         </Link>
       </div>
 
       <div>
-        <span>Inspired by: </span>
-        My special dog Nina, the dog who doesn't go to dog parks.
+        <span>{t('about.inspiredByTitle')} </span>
+        {t('about.inspiredByBody')}
       </div>
       <div>
-        <span>Special thanks to: </span>Kfir Arad, Rotem Koltz.
+        <span>{t('about.specialThanks')}</span>
       </div>
     </div>
   );
