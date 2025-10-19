c2993f2b8ea4a98588a0f4624103fe9147edbc98
esterpratt <esterpratt@gmail.com>
2025-10-19 18:58:33 +0300

    add enums of park event

---
+++ src/types/parkEvent.ts
@@ -0,0 +1,10 @@
+enum ParkEventVisibility {
+  FRIENDS_ALL = 'FRIENDS_ALL',
+  FRIENDS_SELECTED = 'FRIENDS_SELECTED',
+}
+enum ParkEventStatus {
+  ACTIVE = 'ACTIVE',
+  CANCELED = 'CANCELED',
+}
+
+export { ParkEventVisibility, ParkEventStatus };
