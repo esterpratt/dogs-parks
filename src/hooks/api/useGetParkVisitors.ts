fe138feaa2e5644bf3e47de16d07f2fd143e2f48
esterpratt <esterpratt@gmail.com>
2025-11-21 16:53:44 +0200

    fix park visitors stale time to 1 min

--- src/hooks/api/useGetParkVisitors.ts
+++ src/hooks/api/useGetParkVisitors.ts
@@ -3,6 +3,7 @@ import { fetchParkCheckins } from '../../services/checkins';
 import { useUserFriendshipMap } from './useUserFriendshipMap';
 import { getFriendIdsByStatus } from '../../utils/friendship';
 import { FRIENDSHIP_STATUS } from '../../types/friendship';
+import { ONE_MINUTE } from '../../utils/consts';
 
 const useGetParkVisitors = (parkId: string, userId?: string | null) => {
   const { data: visitorsIds = [], isLoading: isLoadingVisitors } = useQuery({
@@ -11,27 +12,29 @@ const useGetParkVisitors = (parkId: string, userId?: string | null) => {
       const checkins = await fetchParkCheckins(parkId);
       return checkins ? checkins.map((checkin) => checkin.user_id) : [];
     },
-    staleTime: 6000,
-    gcTime: 6000,
+    staleTime: ONE_MINUTE,
+    gcTime: ONE_MINUTE,
   });
 
-  const {
-    data: friendshipMap,
-    isLoading: isLoadingFriendshipMap,
-  } = useUserFriendshipMap(userId);
+  const { data: friendshipMap, isLoading: isLoadingFriendshipMap } =
+    useUserFriendshipMap(userId);
 
-  const friendIds = friendshipMap ? getFriendIdsByStatus({
-    friendshipMap,
-    status: FRIENDSHIP_STATUS.APPROVED,
-  }): [];
+  const friendIds = friendshipMap
+    ? getFriendIdsByStatus({
+        friendshipMap,
+        status: FRIENDSHIP_STATUS.APPROVED,
+      })
+    : [];
 
-  const friendsInParkIds = (visitorsIds as string[]).filter(id => friendIds.includes(id));
+  const friendsInParkIds = (visitorsIds as string[]).filter((id) =>
+    friendIds.includes(id)
+  );
 
   return {
     visitorsIds,
     friendsInParkIds,
     isLoadingVisitors,
-    isLoadingFriendIds: (!!userId && isLoadingFriendshipMap),
+    isLoadingFriendIds: !!userId && isLoadingFriendshipMap,
   };
 };
 
